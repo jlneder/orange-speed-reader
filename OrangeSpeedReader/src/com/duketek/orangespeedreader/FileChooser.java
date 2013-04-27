e97d93c9cdbe11e8054487f3e18f7b0dfc2b6e4f
jsduke <jsduke@gmail.com>
2013-04-26 23:38:56 -0400

    Re init git using windows git
    eclipse git was not working so switched to win git command line
    also includes the implementation of a filechooser for opening ebooks.

---
+++ OrangeSpeedReader/src/com/duketek/orangespeedreader/FileChooser.java
@@ -0,0 +1,87 @@
+package com.duketek.orangespeedreader;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ipaulpro.afilechooser.utils.FileUtils;
+
+public class FileChooser extends Activity {
+
+	private static final int REQUEST_CODE = 6384; // onActivityResult request
+	// code
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		showChooser();
+	}
+
+	private void showChooser() {
+		// Use the GET_CONTENT intent from the utility class
+		Intent target = FileUtils.createGetContentIntent();
+		// Create the chooser Intent
+		Intent fileOpenIntent = Intent.createChooser(target, getString(R.string.title_activity_file_chooser));
+		
+		try {
+			startActivityForResult(fileOpenIntent, REQUEST_CODE);
+			target.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			finish();
+			
+			
+		} catch (ActivityNotFoundException e) {
+			// The reason for the existence of aFileChooser
+		}
+
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQUEST_CODE:
+			// If the file selection was successful
+			if (resultCode == RESULT_OK) {
+				if (data != null) {
+					// Get the URI of the selected file
+					final Uri uri = data.getData();
+
+					try {
+						// Create a file instance from the URI
+						final File file = FileUtils.getFile(uri);
+						
+						//save the selected file path
+						String filePath =  file.getAbsolutePath();
+						
+						Toast.makeText(FileChooser.this,
+								"File Selected: " + file.getAbsolutePath(),
+								Toast.LENGTH_LONG).show();
+						//start the word player using the picked file
+						Intent wordPlayerIntent = new Intent(getApplicationContext(), WordPlayerActivity.class);
+						SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+						SharedPreferences.Editor editor = new mPrefs.edit();
+						startActivity(wordPlayerIntent);
+						
+					} catch (Exception e) {
+						Log.e("FileSelectorTestActivity", "File select error",
+								e);
+					}
+				}
+			}
+			break;
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+}
---
+++ OrangeSpeedReader/src/com/duketek/orangespeedreader/ImageAdapter.java
@@ -0,0 +1,142 @@
+package com.duketek.orangespeedreader;
+
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import com.duketek.orangespeedreader.R;
+import com.duketek.orangespeedreader.R.id;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class ImageAdapter extends BaseAdapter {
+
+	Context mContext;
+	
+	// Constructor
+	public ImageAdapter(Context c){
+		mContext = c;
+	 
+	}
+
+	
+	
+	@Override
+	public int getCount() {
+		return btnPics.length;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return btnPics[position];
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+
+//	public void LinearLayout(Context c) {
+//		        mContext = c;
+//	}
+		
+		
+	@Override
+	// create a new ImageView for each item referenced by the Adapter
+	
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		//get the item corresponding to position and store in a BtnBox
+
+		View v = convertView; 
+		//LinearLayout mLinearLayout = findViewById(R.layout.grid_item);
+		ImageView mImageView = null;
+		TextView mTextView = null;
+		
+	    if (convertView == null) { // if it's not recycled, initialize some
+	                                // attributes
+	    LayoutInflater vi = 
+	            (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	            v = vi.inflate(R.layout.grid_item, null);
+	     } 
+	        
+	    mImageView =(ImageView)v.findViewById(R.id.btnPic);
+	    mTextView = (TextView)v.findViewById(R.id.btnText);
+	    
+	    mImageView.setImageResource(btnPics[position]);
+	    mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+	    mImageView.setPadding(8, 8, 8, 8);
+	    
+	    mTextView.setText(btnText[position]);
+	    
+	    return v;		
+		
+		
+		
+		
+//		BtnBox mBtnBox;
+//		
+//		if (convertView == null){
+//			convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_item, parent, false);
+//			mBtnBox = new BtnBox();
+//			mBtnBox.mImageView=(ImageView)convertView.findViewById(R.id.btnPic);
+//			mBtnBox.mTextView=(TextView)convertView.findViewById(R.id.btnText);
+//			mBtnBox.mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//			mBtnBox.mImageView.setPadding(8,8,8,8);
+//			convertView.setTag(mBtnBox);
+//		}
+//		else{
+//			mBtnBox = (BtnBox)convertView.getTag();
+//		}
+//		
+//		mBtnBox.mImageView.setImageResource(btnPics[position]);
+//		mBtnBox.mTextView.setText(btnText[position]);
+//		
+//		return convertView;
+		    
+	}
+		    	
+	    	
+	        //ImageView imageView;
+//	        if (convertView == null) {  // if it's not recycled, initialize some attributes
+//	            
+//	        	imageView = new ImageView(mContext);
+//	            imageView.setLayoutParams(new GridView.LayoutParams(85, 185));
+//	            imageView.setScaleType(ImageView.ScaleType.CENTER);
+//	            imageView.setPadding(8, 8, 8, 8);
+//	        } else {
+//	            imageView = (ImageView) convertView;
+//	        }
+//
+//	        imageView.setImageResource(buttonID[position]);
+//	        return imageView;
+//}
+	    
+	   
+	    
+	    // add pictures to gridshow pretty pictures on the main activity
+	private Integer[] btnPics = { 	R.drawable.play, R.drawable.sd_card, 
+									R.drawable.collection, android.R.drawable.ic_menu_preferences, 
+									android.R.drawable.ic_menu_more, android.R.drawable.ic_menu_close_clear_cancel };
+
+	    // add text to layout
+	static final String[] btnText = new String[] { 	"Resume Reading", "Open Book", 
+													"Recent Books", "Settings", 
+													"About", "Exit"};
+
+	    
+}
+
+	
+
+	
+
